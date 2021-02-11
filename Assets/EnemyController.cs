e9597f3654df5091f7f8c05f13db93d38fef7b6c
Elliot <71550907+EJHandley@users.noreply.github.com>
2021-02-11 23:11:38 +0000

    First Prototype
    Created basic level design, created enemy,  baked navmesh, created AI logic for enemy to use navmesh to navigate level, introduce startpoint and endpoint, create basic turret model

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    public float lookRadius = 10f;
+
+    Transform target;
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        target = EnemyObjective.instance.enemyObjective.transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        agent.SetDestination(target.position);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+}
---
+++ Assets/EnemyObjective.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyObjective : MonoBehaviour
+{
+    #region Singleton
+
+    public static EnemyObjective instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    #endregion
+
+    public GameObject enemyObjective;
+
+}
